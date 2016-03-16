2eca5141c1ed82987f9a406d18425f875120651f
risb0 <bponcet7@gmail.com>
2016-03-16 13:17:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/risbo/pokedex/PokedexActivity.java
@@ -0,0 +1,25 @@
+package com.example.risbo.pokedex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import stanford.androidlib.SimpleActivity;
+
+
+public class PokedexActivity extends SimpleActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokedex);
+    }
+
+
+    public void pokemonClick(View view) {
+
+
+    }
+}
+
+
