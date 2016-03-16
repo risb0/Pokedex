21e5e7bfccecfff7572bd907722f04f3c5a5b27f
risb0 <bponcet7@gmail.com>
2016-03-16 17:38:15 +0100

    Added Intent to pass the matching image from Main to Details activity

--- app/src/main/java/com/example/risbo/pokedex/DetailsActivity.java
+++ app/src/main/java/com/example/risbo/pokedex/DetailsActivity.java
@@ -1,13 +1,39 @@
 package com.example.risbo.pokedex;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
-public class DetailsActivity extends AppCompatActivity {
+import stanford.androidlib.SimpleActivity;
 
+public class DetailsActivity extends SimpleActivity {
+    private int pokeIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        Intent intent = getIntent();
+        pokeIcon = intent.getIntExtra("pokeImageDrawable",-1);
+
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("debug", "pokeIcon = " + pokeIcon);
+
+
+        ImageView pokeView = (ImageView) findViewById(R.id.pokemon_image);
+
+        pokeView.setImageResource(pokeIcon);
+
     }
 }
--- app/src/main/java/com/example/risbo/pokedex/PokedexActivity.java
+++ app/src/main/java/com/example/risbo/pokedex/PokedexActivity.java
@@ -1,13 +1,21 @@
 package com.example.risbo.pokedex;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 
 import stanford.androidlib.SimpleActivity;
 
 
 public class PokedexActivity extends SimpleActivity {
+
+    private Drawable pokeIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,21 @@ public class PokedexActivity extends SimpleActivity {
 
     public void pokemonClick(View view) {
 
+
+
+        int imageId = view.getId();
+
+        ImageButton pokeImage = (ImageButton) findViewById(imageId);
+       // Log.d("tag",String.valueOf(imageId));
+
+        int pokeImageDrawable =  getResources().getIdentifier(pokeImage.getTag().toString(), "drawable", getApplicationContext().getPackageName());
+
+        Log.d("tag", "pokeImageDrawable = "  + pokeImageDrawable);
+
+
+        Intent intent = new Intent(this,DetailsActivity.class);
+        intent.putExtra("pokeImageDrawable",pokeImageDrawable);
+        startActivity(intent);
 
     }
 }
